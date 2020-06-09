ea8c2d176735454783db906cbdde5ac25e96dc74
snh3003 <shahsama542@gmail.com>
2020-06-09 06:31:32 +0530

    Updates

---
+++ precache-manifest.3f103716a89bea25d78e26022db780c2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec640d206201d016cc161f2cbe5736f0",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "ca9fdd2a0d3f4c3b9389",
+    "url": "/todolist/static/js/2.603c9526.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todolist/static/js/2.603c9526.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9317774b3f24e8b6fd66",
+    "url": "/todolist/static/js/main.2c12e422.chunk.js"
+  },
+  {
+    "revision": "94e2713e27bcde79676a",
+    "url": "/todolist/static/js/runtime-main.86e7f888.js"
+  }
+]);
